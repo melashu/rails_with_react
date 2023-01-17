5f9559f0212f50c45b7e8db6daa426ca46b3dfcd
melashu <meshu102@outlook.com>
2023-01-17 14:53:56 +0300

    Add greeting controller

---
+++ app/controllers/api/v1/greets_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetsController < ApplicationController
+      def index
+        result = Greeting.all
+        render json: result.to_json({ only: %i[message id] })
+      end
+    end
+  end
+end
