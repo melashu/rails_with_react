7df18337faf165a6822d670f4baf0a363626dced
melashu <meshu102@outlook.com>
2023-01-17 15:05:51 +0300

    Add linter fix

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class RootsController < ApplicationController
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # get 'roots/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get 'greets', to: 'greets#index', as: 'greets_path'
+    end
+  end
+  root 'roots#index'
 end
